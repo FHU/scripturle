8f17adcfb54c2014e3c96a87a85491faa89ad36c
SeanJaniec <78286187+SeanJaniec@users.noreply.github.com>
2022-04-21 19:29:05 -0500

    changes

---
+++ ThyWordle/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <guess></guess>
+  </div>
+</template>
+
+<script>
+import Guess from './components/Guess.vue';
+
+export default {
+  components: {
+    'guess': Guess
+  },
+  
+  
+  name: 'app',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ ThyWordle/src/components/Guess.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="guess">
+      <h2>Guess</h2>
+      <ul>
+        <li v-for=""></li>
+      </ul>
+    
+    
+    </div>
+    
+</template>
+
+<script>
+import Letter from './components/Letter.vue';
+
+</script>
+
+<style>
+
+</style>
